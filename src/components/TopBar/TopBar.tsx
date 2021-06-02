cd905f07b83ecb963c4ce5ed2616305460786132
tofutree23 <tofutree23@gmail.com>
2021-06-02 17:00:31 +0900

    add: topbar

---
+++ src/components/TopBar/TopBar.tsx
@@ -0,0 +1,5 @@
+const TopBar = () => {
+    return <></>
+}
+
+export default TopBar
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Root } from './Root'
 export { default as Greeting } from './Greeting'
 export { default as BottomBar } from './BottomBar'
+export { default as TopBar } from './TopBar'
