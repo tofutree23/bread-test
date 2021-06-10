d9e2d91de99e2f8c4bda2617c3b80c60872a7dd5
Jonghyung <tofutree23@gmail.com>
2021-06-10 17:44:24 +0900

    add: rough result

--- src/components/InnerContents/Result/Result.tsx
+++ src/components/InnerContents/Result/Result.tsx
@@ -1,21 +1,32 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { createUseStyles } from 'react-jss'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 
 import ContentsBase from '@/components/ContentsBase'
+
+import { BREAD } from '@/res/contents/result'
 
 interface ResultProps {}
 
 const Result: FC<ResultProps> = ({ ...props }) => {
+  const history = useHistory()
+  const [breadName, setBreadName] = useState<string>('')
+
   const { bread } =
     useParams<{
       bread: string
     }>()
 
+  useEffect(() => {
+    const breadName = Object.values(BREAD).find(value => value === bread)
+    if (!breadName) return history.push('/404')
+    setBreadName(breadName)
+  }, [bread, history])
+
   const classes = useStyles()
   return (
     <ContentsBase>
-      <div className={classes.resultWrapper}>RESULT {bread}</div>
+      <div className={classes.resultWrapper}>RESULT {breadName}</div>
     </ContentsBase>
   )
 }
--- src/res/contents/result.ts
+++ src/res/contents/result.ts
@@ -1,1 +1,18 @@
-export const result: string[] = []
+export const BREAD = {
+  intj: '인티제',
+  intp: '인팁',
+  entj: '엔티제',
+  entp: '엔팁',
+  infj: 'baguette',
+  infp: '인프피',
+  enfj: '엔프제',
+  enfp: '엔프피',
+  istj: '잇티제',
+  isfj: '잇프제',
+  estj: '엣티제',
+  esfj: '엣프제',
+  istp: '잇팁',
+  isfp: '잇프피',
+  estp: '엣팁',
+  esfp: '엣프피'
+}
